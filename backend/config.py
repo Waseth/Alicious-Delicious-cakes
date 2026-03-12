c1f6cfc13aebd958af6274a6aee1822b7fd6faa0
Waseth <wasethsapriso@gmail.com>
2026-03-13 02:33:11 +0300

    backend run complete

--- backend/config.py
+++ backend/config.py
@@ -14,7 +14,7 @@ class Config:
 
     DB_HOST = os.getenv("DB_HOST", "localhost")
     DB_PORT = os.getenv("DB_PORT", "3306")
-    DB_NAME = os.getenv("DB_NAME", "alicious_delicious_db")
+    DB_NAME = os.getenv("DB_NAME", "alicious_cakes_db")
     DB_USER = os.getenv("DB_USER", "root")
     DB_PASSWORD = os.getenv("DB_PASSWORD", "")
     SQLALCHEMY_DATABASE_URI = (
